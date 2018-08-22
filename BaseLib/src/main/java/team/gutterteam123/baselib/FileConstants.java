3a0a426d7f5d0c7e18d24d7fd8ea048e18334903
david <davidscandurra@gmail.com>
2018-08-22 03:20:26 +0200

    Fixed Java Convensions

--- BaseLib/src/main/java/team/gutterteam123/baselib/FileConstants.java
+++ BaseLib/src/main/java/team/gutterteam123/baselib/FileConstants.java
@@ -6,10 +6,10 @@ import java.io.File;
 
 public final class FileConstants {
 
-    @Getter private static final File dinstagram = new File(System.getProperty("user.home"), "Dinstagram/");
-    @Getter private static final File repo = new File(dinstagram, "repo/");
+    @Getter private static final File DINSTAGRAM = new File(System.getProperty("user.home"), "Dinstagram/");
+    @Getter private static final File REPO = new File(DINSTAGRAM, "repo/");
 
-    @Getter private static final File config = new File(dinstagram, "config.json");
-    @Getter private static final File configServer = new File(dinstagram, "configserver/config.json");
+    @Getter private static final File CONFIG = new File(DINSTAGRAM, "config.json");
+    @Getter private static final File CONFIG_SERVER = new File(DINSTAGRAM, "configserver/config.json");
 
 }
