6b79297bb04dd6aa07593d84ace97c9da7fba57c
DominikBi <34425521+DominikBi@users.noreply.github.com>
2018-09-05 06:41:43 +0000

    Update JsonPacket.java

--- NetLib/src/main/java/team/gutterteam123/netlib/packetbase/json/JsonPacket.java
+++ NetLib/src/main/java/team/gutterteam123/netlib/packetbase/json/JsonPacket.java
@@ -3,7 +3,4 @@ package team.gutterteam123.netlib.packetbase.json;
 import team.gutterteam123.netlib.packetbase.Packet;
 
 public interface JsonPacket extends Packet {
-  @Getter @Setter
-  Packet packet = new Packet(); 
-
 }
