c78e70fa8f910970895dbb1b4da8d6e3206206b6
DominikBi <34425521+DominikBi@users.noreply.github.com>
2018-09-25 09:55:28 +0000

    Update main.js

--- Client/static/main.js
+++ Client/static/main.js
@@ -1,1 +1,13 @@
+var connectionList = [];
 
+function ConnectToServer(URL...){
+connectionList.clear();
+connectionList.concat(URL);
+var socket = new io.Socket();
+socket.connect(connectionList.pop(RandomNumber(connectionList.length)));
+}
+
+
+function RandomNumber(max){
+ return Math.random()*max; 
+}
