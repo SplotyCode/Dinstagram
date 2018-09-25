c1516db18d6d3455b76e049b7273698136820d48
DominikBi <birkeldominik@gmail.com>
2018-09-25 21:33:00 +0200

    main js file edited

--- Client/static/main.js
+++ Client/static/main.js
@@ -1,12 +1,31 @@
-var connectionList = [];
+const io = require('socket.io-client');
+import io from 'socket.io-client';
 
-function ConnectToServer(URL...){
+var Server = {
+    ConnectingStanding = false,
+    URLConnected = ""
+
+}
+function ConnectToServer(URL){
+var connectionList = [URL.length];
 connectionList.clear();
 connectionList.concat(URL);
-var socket = new io.Socket();
-socket.connect(connectionList.pop(RandomNumber(connectionList.length)));
+var socket = io.connect(connectionList.pop(RandomNumber(connectionList.length)))
+Server.ConnectingStanding = true;
+Server.URLConnected = URL;
 }
+function ReadFile(Filepath, ConnetionServer){
+    ConnectToServer(ConnetionServer);
+    fs.ReadFile(Filepath, function read(err,data){
+        if(err){
+            throw err;
+        }
+        return data;
+    })
 
+
+
+}
 
 function RandomNumber(max){
  return Math.random()*max; 
